9a6afe0f22fef623d554185540d8702d93524687
Guil H <g.hernandez07@gmail.com>
2023-02-23 15:21:34 -0500

    Added starter files

---
+++ index.js
@@ -0,0 +1,13 @@
+// Product A info
+let productA = {
+    emoji: "⭐",
+    revenue: 200,
+    commision: 50
+}
+
+// Product B info
+let productB = {
+    emoji: "🔥",
+    revenue: 300,
+    commision: 75
+}
