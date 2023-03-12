8129b23b27d299f7f1379c3377222b216f94e648
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-03-12 18:21:09 -0400

    Update: Calculated Total Revenue and updated HTML content for the total-revenue

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
+// Product A info
+let productA = {
+  emoji: "⭐",
+  revenue: 200,
+  commision: 50,
+};
+
+// Product B info
+let productB = {
+  emoji: "🔥",
+  revenue: 300,
+  commision: 75,
+};
+
 const starProductBtn = document.getElementById("star-product");
 const fireProductBtn = document.getElementById("fire-product");
 const soldProducts = document.getElementById("sold-products");
 const achievements = document.getElementById("achievements");
+
+const revenueArr = [];
 
 function handleClick() {
   document.addEventListener("click", (e) => {
     e.preventDefault();
     switch (e.target.dataset.product) {
       case "star":
-        soldProducts.innerHTML += "⭐";
+        soldProducts.innerHTML += productA.emoji;
+        updateRevenueArr(productA.revenue);
         break;
       case "fire":
-        soldProducts.innerHTML += "🔥";
+        soldProducts.innerHTML += productB.emoji;
+        updateRevenueArr(productB.revenue);
         break;
       default:
         break;
     }
     addBellIconOnProductBtnClick();
+    updateHtmlForTotalRevenue();
   });
 }
 
 handleClick();
 
 function addBellIconOnProductBtnClick() {
-  if (soldProducts.innerHTML === "⭐" || soldProducts.innerHTML === "🔥") {
+  if (
+    soldProducts.innerHTML === productA.emoji ||
+    soldProducts.innerHTML === productB.emoji
+  ) {
     achievements.innerHTML = "🔔";
   }
+}
+
+function updateHtmlForTotalRevenue() {
+  document.getElementById("total-revenue").textContent =
+    calculateTotalRevenue();
+}
+
+function calculateTotalRevenue() {
+  return revenueArr.reduce(
+    (totalRevenue, revenue) => totalRevenue + revenue,
+    0
+  );
+}
+
+function updateRevenueArr(revenue) {
+  revenueArr.push(revenue);
 }
