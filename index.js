290901ea678d5ba9e26d2f378d471c9a4ced435d
pixelliquid <peter@pixelliquid.com>
2023-03-17 14:56:04 +0100

    some housekeeping: cleanup logs and comments

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Salesboard App
-
 let totalSales = ''
 let timesClicked = 0
 let achievementsUnlocked = 0
@@ -7,7 +6,6 @@ let achievementsBadge = ''
 let achievementsBadgeTwo = false
 let totalRevenue = 0
 let totalCommission = 0
-
 let productA = {
     emoji: "⭐",
     revenue: 200,
@@ -19,8 +17,16 @@ let productB = {
     commission: 75
 }
 
+// Light and dark mode switch
+const toggleBtn = document.querySelector('.toggle-mode')
+toggleBtn.addEventListener('click', function () {
+    document.body.classList.toggle('light')
+    toggleBtn.classList.toggle('fa-moon')
+    toggleBtn.classList.toggle('fa-sun')
+})
+
 // Setup
-// Some interaction: the sales buttons
+// The sales buttons
 const paBtn = document.getElementById('btn-productA')
 const pbBtn = document.getElementById('btn-productB')
 // Show the correct emoji on those sales buttons
@@ -44,25 +50,17 @@ const commissionBar = document.getElementById('commission-bar')
 
 renderSales()
 
-// Sales
-// Clicking the sales buttons will:
-// 1. update totalSales array with sales
-// 2. update "Live Sales" in the HTML with the content of the totalSales array
-// 3. update "Live Sales" with times either of the two buttons is clicked
-// 4. update totalRevenue and totalCommission with values set in respective product arrays
-// 5. update totalRevenue and totalCommission in the HTML with the content of their arrays
+// Sales button actions
 function fixSale(salesProduct) {
-    // console.log(`product A ${productA.emoji}`) // check
     totalSales += salesProduct.emoji
     totalRevenue += salesProduct.revenue
     totalCommission += salesProduct.commission
     timesClicked += 1
-    console.log(totalSales, totalRevenue, totalCommission, timesClicked) // check
     checkAchievements()
     renderSales()
 }
 
-// Show stuff in separate function for clarity
+// Show sales
 function renderSales() {
     salesHeader.textContent = `Live Sales - ${timesClicked}`
     salesBar.textContent = totalSales
@@ -72,36 +70,17 @@ function renderSales() {
     commissionBar.textContent = `$ ${totalCommission}`
 }
 
-
-// Achievements
-// The achievements are given on three occasions:
-// 1. With first sale (when timesClicked === 1)
-// 2. When revenue reaches $2500 (when totalRevenue >= 2500) - beware: this should ONLY happen once only when this goal is reached. Boolean to set?
-// 3. With the 15th sale (when timesClicked === 15)
-// First and third goal are easy, second goal has more to it.
-// I like to make this happen with just one variable but in first iteration I'll use a boolean which switches when goal is met
+// Achievements logic
 function checkAchievements() {
     if (timesClicked === 1) {
         achievementsUnlocked += 1
         achievementsBadge += '🔔'
-        console.log(achievementsBadge) // check
     } else if (totalRevenue >= 2500 && achievementsBadgeTwo === false) {
         achievementsUnlocked += 1
         achievementsBadge += '💰'
         achievementsBadgeTwo = true
-        console.log(achievementsBadgeTwo) // check
-        console.log(achievementsBadge) // check
     } else if (timesClicked === 15) {
         achievementsUnlocked += 1
         achievementsBadge += '🏆'
-        console.log(achievementsBadge) // check
     }
 }
-
-//Light and dark mode event listener
-const toggleBtn = document.querySelector('.toggle-mode')
-toggleBtn.addEventListener('click', function() {
-    document.body.classList.toggle("light")
-    toggleBtn.classList.toggle("fa-moon")
-    toggleBtn.classList.toggle("fa-sun")
-})
